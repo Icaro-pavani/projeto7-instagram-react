cd2d79c1478c6d87c26ed8317f91f27cf0a4c949
Icaro-pavani <icaro.pavani79@gmail.com>
2022-03-11 17:32:41 -0300

    Sessao main finalizada

--- src/Ajuda.js
+++ src/Ajuda.js
@@ -3,7 +3,7 @@ export default function Ajuda() {
         <div class="ajuda">
           <p>
             <a href="...">Sobre</a> • <a href="...">Ajuda</a> • <a href="...">Imprensa</a> • <a href="...">API</a> • <a href="...">Carreiras</a> • <a href="...">Privacidade</a> • <a href="...">Termos</a> •
-            <a href="...">Localizações</a> • <a href="...">Contas mais relevantes</a> • <a href="...">Hashtags</a> • <a href="...">Idioma</a>
+            <a href="..."> Localizações</a> • <a href="...">Contas mais relevantes</a> • <a href="...">Hashtags</a> • <a href="...">Idioma</a>
           </p>
         </div>
     );
--- src/index.js
+++ src/index.js
@@ -1,33 +1,15 @@
 import ReactDOM from 'react-dom';
 import Header from './Header';
+import Main from "./Main";
 
 import Feeds from "./Feeds";
-import PerfilFonte from "./PerfilFonte";
-import Sugestoes from "./Sugestoes";
-import Ajuda from "./Ajuda";
-import Company from "./Company"
+import Sidebar from "./Sidebar";
 
 function App() {
     return (
         <div>
             <Header />
-            <main>
-                <Feeds />
-                <div className="sidebar">
-                    <PerfilFonte name="catanacomics" nomeExibido="Catana" />
-
-                    <Sugestoes />
-
-                    {/* <ul className="sugestoes">
-                        <TopoSugestoes />
-
-                        <Sugestao name="badvibesmemes" nomeExibido="bad.vibes.memes" seguir="sim"/>
-                    </ul> */}
-
-                    <Ajuda />
-                    <Company ano="2022" />
-                </div>
-            </main>
+            <Main />
         </div>
     );
 }
